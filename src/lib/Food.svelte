47dd149829ade3f5fcde6b316b716821b967c1a0
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2021-12-22 18:56:42 -0500

    food cycling

--- src/lib/Food.svelte
+++ src/lib/Food.svelte
@@ -1,7 +1,21 @@
 <script>
+  import { onDestroy } from 'svelte';
+  import { fly } from 'svelte/transition'
+
   const foods = ['🦃','🎂','🍖','🍞']
+  let curr = 0
+  const int = setInterval(() => {
+    curr++
+    curr %= 4
+  }, 3000)
+
+  onDestroy(()=>clearInterval(int))
 </script>
 
-<div class='inline'>
-  <span>{foods[0]}</span>
+<div class='inline ml-2'>
+  {#each foods as food}
+    {#if food === foods[curr]}
+      <span class="inline-block absolute" in:fly={{y:36}} out:fly={{y:-36}}>{food}</span>
+    {/if}
+  {/each}
 </div>
